b85e6c238b87d73dd2708c6116c57e82f6b66b1b
ci-gitlab <caojianbo@xd.com>
2021-02-04 14:47:45 +0800

    Revert "Revert "Revert "change title"""
    This reverts commit 17c55898bfb1203176fcf2f6a2d3a851a7f7727a.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,9 +10,9 @@ module.exports = {
   projectName: 'XDSDK-Doc', // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: 'XDSDK 文档中心',
+      title: 'My Site',
       logo: {
-        alt: 'XDSDK 文档中心',
+        alt: 'My Site Logo',
         src: 'img/logo.svg',
       },
       items: [
@@ -31,6 +31,53 @@ module.exports = {
       ],
     },
     footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Style Guide',
+              to: 'docs/',
+            },
+            {
+              label: 'Second Doc',
+              to: 'docs/doc2/',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+            },
+            {
+              label: 'Discord',
+              href: 'https://discordapp.com/invite/docusaurus',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/docusaurus',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: 'blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/facebook/docusaurus',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
     },
   },
   presets: [
@@ -38,11 +85,16 @@ module.exports = {
       '@docusaurus/preset-classic',
       {
         docs: {
-          routeBasePath: "/",
           sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/',
         },
         blog: {
           showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
