f0d22318aad7c78c22b0302fbb60f9743185a3f5
ci-gitlab <caojianbo@xd.com>
2021-02-04 13:44:56 +0800

    chang title

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-  title: 'My Site',
+  title: 'XDSDK 文档中心',
   tagline: 'The tagline of my site',
   url: 'https://xindong.github.io',
   baseUrl: '/XDSDK-Doc/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'xindong', // Usually your GitHub org/user name.
+  projectName: 'XDSDK-Doc', // Usually your repo name.
   themeConfig: {
     navbar: {
       title: 'My Site',
